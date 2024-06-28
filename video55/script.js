cd397e4f81b3d96a8aad0bc2f229607c0690e484
Bhimani Yash Shaileshbhai <161473598+YashGaara@users.noreply.github.com>
2024-06-28 19:18:13 +0530

    java script work start

---
+++ video55/script.js
@@ -0,0 +1,26 @@
+console.log("hey this tutorial 55")
+
+var a=5;
+var b=6;
+var c="harry";
+let name="yash";
+
+console.log(a + b + 8)
+console.log(typeof a,typeof b, typeof c)
+
+//gconst a1=6;
+// a1=a1+1; //not allowed 
+
+let o = {
+    "name": "Harry",
+    "job code": 5600,
+    "is_handsome": true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
+
+
