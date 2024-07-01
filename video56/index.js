7e2f57397b4b4cf68716a242dcae3cb626cb5ed6
Bhimani Yash Shaileshbhai <161473598+YashGaara@users.noreply.github.com>
2024-07-01 23:33:13 +0530

    to

---
+++ video56/index.js
@@ -0,0 +1,10 @@
+console.log("hello i am conditonal tutorial")
+let age = 16;
+let grace = 2;
+
+if(age+grace   > 18){
+  console.log("you can drive")
+}
+else{
+  console.log("you cannot drive")  
+}
