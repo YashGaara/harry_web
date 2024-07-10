6081471b91650d01bacbe3e8fadae122b2bc1838
Bhimani Yash Shaileshbhai <161473598+YashGaara@users.noreply.github.com>
2024-07-10 18:48:22 +0530

    day

--- video56/index.js
+++ video56/index.js
@@ -1,10 +1,28 @@
-console.log("hello i am conditonal tutorial")
-let age = 16;
-let grace = 2;
-
-if(age+grace   > 18){
-  console.log("you can drive")
-}
-else{
-  console.log("you cannot drive")  
-}
+function nice(name){
+    console.log("Hey "+name + " you are nice!")
+    console.log("Hey "+ name + " you are nice!")
+    console.log("Hey "+ name +" you are nice!")
+    console.log("Hey "+ name +" you are nice!")  
+  }
+  
+  nice("yash")
+  
+  function sum(a,b, c=3){
+  //console.log(a+b)
+    return a + b + c
+  }
+  result1 = sum (3,5)
+  result2 = sum (7,5)
+  result3 = sum (3,13,1)
+  
+  console.log("the sum of these number: ", result1)
+  console.log("the sum of these number: ", result2)
+  console.log("the sum of these number: ", result3)
+  
+  const func1 = (x)=>{
+    console.log("I am an arrow function", x)
+  }
+  
+  func1(34);
+  func1(66);
+  func1(84);
